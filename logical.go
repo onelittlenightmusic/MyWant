57dcccb9d9f4fbe0a6f922a8a496a92efdc9328b
hiroyuki.osaki@gmail.com <onelittlenight>
2015-09-28 16:26:17 +0000

    second added

---
+++ logical.go
@@ -0,0 +1,71 @@
+package main
+import( "fmt" )
+type m struct {
+	lowend string;
+	highend string;
+	pole *string;
+}
+type area struct {
+	low_end int;
+	high_end int;
+}
+func load() {
+	//fileからの読み込みを実装
+	;
+}
+func write() {
+	//fileへの書き込みを実装
+	;
+}
+func mece(ideas []area, target area) ([]area) {
+	//input: ideas 同列のアイディアの配列、target 目的
+	//軸を見つける
+	//モレを見つける
+		//全体範囲を目的から抽出
+		//a:=上限と下限を持つstructのarray
+		a:= make([]area, 10);
+		i:= 0;
+		//ideaを下限でソート
+/*		if target.low_end < ideas[0].low_end {
+			//aに、最低の下限値でカバーできてない範囲を追加
+			a[i] = area{target.low_end, ideas[0].low_end};
+			i++;
+		}
+		*/
+//		a[0] = target;
+		max_high := target.low_end;
+		for _, idea := range ideas {
+			if max_high < idea.low_end {
+				//モレ発見
+				a[i] = area{max_high, idea.low_end};
+				i++;
+			}
+			if max_high < idea.high_end {
+				max_high = idea.high_end;
+			}
+		}
+		if max_high < target.high_end {
+			//aに、最大の上限値でカバーできてない範囲を追加
+			a[i] = area{max_high, target.high_end};
+			i++;
+		}
+		
+		//ダブりを見つける
+	return a;
+}
+func main() {
+	poles := make([]string, 10);
+	a := m{"","",&poles[0]};
+	n := make([]area, 10);
+	n[0] = area{10,20};
+	n[1] = area{20,30};
+	x := mece(n, area{0,100});
+	poles[0] = "new";
+	fmt.Printf("%s\n", poles[0]);
+	fmt.Printf("%T\n", poles);
+	fmt.Printf("%s\n", *(a.pole));
+	for i, x1 := range x {
+		fmt.Printf("[%d](%d, %d)\n", i, x1.low_end, x1.high_end);
+	}
+}
+	
---
+++ ortho_array.go
@@ -0,0 +1,106 @@
+package main
+import (
+	"fmt";
+)
+
+
+type tupple struct {
+	num int;
+	rownum int;
+	row [32]int;
+}
+func double(in, out chan tupple) {
+	n := 0;
+	for {
+		select {
+		case x := <- in:
+			if x.num<0 { _ = out <- x; break; }
+			x.rownum++;
+			x.num = n;
+			n++;
+			x.row[x.rownum-1] = 0;
+			_ =  out <- x;
+			x.num = n;
+			n++;
+			x.row[x.rownum-1] = 1;
+			_ = out <- x;
+		}
+	}
+};
+func plus(in, out chan tupple) {
+//	n := 0;
+	for {
+		select {
+		case x := <- in:
+			if x.num<0 { _ = out <- x; break;}
+			_row_max := x.rownum -1;
+			for i := 0; i < _row_max; i++ {
+				x.rownum++;
+				x.row[x.rownum-1] = (x.row[_row_max] + x.row[i])%2;
+			}
+			out <- x;
+		}
+	}
+};
+func tested(p [32]int) bool {
+	if p[2]>0 && p[4]>0 {
+		panic(fmt.Sprintf("%v, %v", p[2], p[4]));
+		return false;
+	}
+	return true;
+};
+func main() {
+	defer func() { fmt.Printf("defered main\n"); }();
+	a := make(chan tupple, 32);
+	b := make(chan tupple, 32);
+	c := make(chan tupple, 32);
+	d := make(chan tupple, 32);
+	end := make(chan int, 1);
+	//double
+	_loop_max := 3;
+	go double(a, c);
+	go plus(c, b);
+//	go double(c, b);
+	go func() {
+		loop := 0;
+		for {
+			select {
+			case x := <- b:
+				switch {
+				case loop==_loop_max:
+					end <- 1;
+				case x.num<0:
+					loop++;
+					a <- x;
+				case loop>= _loop_max -1:
+					for i:=0; i<x.rownum; i++ {
+						fmt.Printf("%d\t", x.row[i]);
+					}
+					fmt.Printf("\n");
+					d <- x;
+				default:
+					a <- x;
+				}
+			}
+		}
+	}();
+// call tested function
+	go func() {
+		for {
+			select {
+			case x := <- d:
+				func() {
+					defer func() {
+						if r:=recover(); r!=nil { fmt.Printf("recovered tested\n"); }
+					}();
+					tested(x.row);
+				}();
+			}
+		}
+	}();
+	a <- tupple{ 0, 1, [32]int{0} };
+	a <- tupple{ 1, 1, [32]int{1} };
+	a <- tupple{ -1, 1, [32]int{0} };
+	<-end;
+}
+
